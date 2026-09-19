e0725c1816503e2d749e5e9c48ac6b0223f715d4
Raeman's Projects <araiagent222@gmail.com>
2026-09-18 22:36:33 -0700

    Fix background image URL in Home.js

--- myapp/src/Home.js
+++ myapp/src/Home.js
@@ -8,7 +8,7 @@ const App = () => {
   return ( <div
     className="bg-dark text-white vh-100 d-flex align-items-center justify-content-center"
     style={{
-      backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Contemporary_Computer_Lab.jpg/1024px-Contemporary_Computer_Lab.jpg.jpg')`, // Replace with your image URL
+      backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Contemporary_Computer_Lab.jpg/1024px-Contemporary_Computer_Lab.jpg')`, // Replace with your image URL
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       position: 'relative',
